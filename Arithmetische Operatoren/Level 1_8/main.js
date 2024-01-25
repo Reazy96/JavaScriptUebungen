afec86611eeecafc42678016934fb856db9219c0
Reazy96 <resul.sadriu@outlook.com>
2024-01-25 15:46:01 +0100

    lvl 1_8 done

---
+++ Arithmetische Operatoren/Level 1_8/main.js
@@ -0,0 +1,43 @@
+let x = 20;
+let y = 30;
+
+console.log(x + y);
+
+console.log(x - y);
+
+console.log(x * y);
+
+console.log(x / y);
+
+console.log(x % y);
+
+let z = 10;
+
+console.log((x * y) / z);
+
+let resultOne = (x * y) / z;
+console.log(resultOne);
+
+let a = 15;
+let b = 9;
+
+console.log(a % b);
+
+let c = 20;
+
+console.log(a + c * c);
+
+let resultTwo = a + c * c;
+console.log(resultTwo);
+
+a++;
+console.log(a);
+
+b--;
+console.log(b);
+
+console.log(a - b);
+
+let resultThree = a - b;
+
+console.log(resultOne & resultTwo);
