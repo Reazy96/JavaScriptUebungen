1b9014f656cba1637e616c6202e06883e03a2ef4
Reazy96 <resul.sadriu@outlook.com>
2024-01-25 15:06:23 +0100

    lvl 1_6 done

---
+++ Intro/Level 1_6/main.js
@@ -0,0 +1,14 @@
+let x = 34;
+
+x = 67;
+
+console.log(x);
+
+// let ... = kann überschrieben werden //
+
+const y = 34;
+y = 67;
+
+console.log(y);
+
+//ich erhalte eine Fehlermeldung weil const ... =nicht überschrieben werden kann //
