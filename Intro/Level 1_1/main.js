cfa7f1f3ee695ab36939c602c22ab2b6195cf8ce
Reazy96 <resul.sadriu@outlook.com>
2024-01-25 14:25:57 +0100

    lvl 1_1 done

---
+++ Intro/Level 1_1/main.js
@@ -0,0 +1,32 @@
+let a = 12;
+let b = a * 3;
+
+console.log(b);
+
+document.write("<h2>" + b + "</h2>");
+
+let firstName = "Anton";
+
+console.log(firstName);
+
+let age = 28;
+
+console.log(age);
+
+let job = "Trainer";
+console.log(job);
+
+let married = true;
+console.log(married);
+
+console.log(
+  firstName +
+    " ist " +
+    age +
+    " alt und " +
+    "arbeitet als " +
+    job +
+    " und ist " +
+    "verheiratet " +
+    married
+);
