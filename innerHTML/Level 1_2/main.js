e8d86233ca90f3c32f0392a1ab94a25e73fb25cd
Reazy96 <resul.sadriu@outlook.com>
2024-01-26 13:53:00 +0100

    lvl 1_2 done

---
+++ innerHTML/Level 1_2/main.js
@@ -0,0 +1,11 @@
+const info = document.getElementById("info");
+info.innerHTML = "<h1> Hello World </h1>";
+info.innerHTML += "<h2> How are you? </h2>";
+
+console.log(info);
+
+const container = document.getElementById("container");
+container.innerHTML = "<p> start of the element </p>";
+document.write("<p> end of the element</p>");
+
+console.log(container);
