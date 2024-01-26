076588d34b858bdf2004540197f56242ad7b1651
Reazy96 <resul.sadriu@outlook.com>
2024-01-26 14:33:45 +0100

    figgi

---
+++ innerHTML/Level 1_3/main.js
@@ -0,0 +1,8 @@
+const gallery = document.getElementById("gallery");
+
+gallery.innerHTML =
+  "<figure> <img src='./img/madara.jpg'/> <figcaption> Fig.1 </figcaption></figure>";
+gallery.innerHTML +=
+  "<figure> <img src='./img/madara.jpg'/> <figcaption> Fig.2 </figcaption></figure>";
+gallery.innerHTML +=
+  "<figure> <img src='./img/madara.jpg'/> <figcaption> Fig.3 </figcaption></figure>";
