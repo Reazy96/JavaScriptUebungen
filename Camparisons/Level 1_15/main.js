b2bd55be6ee9ac00a7ff17d919abd2e65b271be8
Reazy96 <resul.sadriu@outlook.com>
2024-01-26 15:07:14 +0100

    lvl 1_15 done

---
+++ Camparisons/Level 1_15/main.js
@@ -0,0 +1,12 @@
+let num = 5;
+let anothernum = 8;
+
+console.log(num == anothernum);
+console.log(num === anothernum);
+console.log(num > anothernum);
+console.log(num !== anothernum);
+console.log(num > anothernum);
+console.log(num <= anothernum);
+console.log(num != anothernum);
+console.log(anothernum > num);
+console.log(num >= anothernum);
