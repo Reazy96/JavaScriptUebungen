a9b13e2d8a4f3e1da7fe80e0db928e8abf7a02af
Reazy96 <resul.sadriu@outlook.com>
2024-01-29 00:47:56 +0100

    lvl 1_5 done

---
+++ Funktionen/Level 1_5/main.js
@@ -0,0 +1,10 @@
+function math(item1, item2) {
+  console.log("Multiplikation  " + item1 * item2);
+  console.log("Division " + item1 / item2);
+}
+math(10, 2);
+math(30, 20);
+math(100, 100);
+math(5, 0);
+math(45, 173);
+math(1, 100);
