4928c97eb71d1db99f374e5c803f18c13b41909b
Reazy96 <resul.sadriu@outlook.com>
2024-01-29 00:32:48 +0100

    lvl 1_3 done

---
+++ Funktionen/Level 1_3/main.js
@@ -0,0 +1,15 @@
+function intro3(name, stadt, alter) {
+  console.log(
+    "Hallo, mein Name ist " +
+      name +
+      ". " +
+      "Ich bin " +
+      alter +
+      " Jahre alt. " +
+      "Ich komme aus " +
+      stadt +
+      "."
+  );
+}
+
+intro3("Reazy", "Limburg", "27");
