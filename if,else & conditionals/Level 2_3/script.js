1f80e51d2383785c6421291fdcd57ab4ca7bea8c
Reazy96 <resul.sadriu@outlook.com>
2024-01-31 21:28:21 +0100

    lvl 2_3 done

---
+++ if,else & conditionals/Level 2_3/script.js
@@ -0,0 +1,13 @@
+function difference(randomNr) {
+  if (randomNr > 27) {
+    let calculation = randomNr - 27;
+    let result = calculation * 2;
+    console.log(calculation);
+    console.log(result);
+  }
+}
+
+difference(100);
+difference(35);
+difference(74);
+difference(123);
