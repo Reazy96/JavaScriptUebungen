4af355e31a30965648d9d00705dbd8e69df4a325
Reazy96 <resul.sadriu@outlook.com>
2024-02-02 13:34:10 +0100

    lvl 1_2 done

--- Ternary Operator/Project Nachrichtenformular/script.js
+++ Ternary Operator/Project Nachrichtenformular/script.js
@@ -1,11 +1,11 @@
 function message() {
   let text = document.querySelector("#typeText").value;
-  let feedback = document.querySelector("h2");
+  let transfer = document.querySelector("h2");
   let warning = document.querySelector("h3");
 
-  text.length >= 1
-    ? (feedback.innerHTML = text)
-    : (warning.innerHTML = "Bitte gib ein Nachricht ein");
+  let feedback =
+    text.length >= 1
+      ? (transfer.innerHTML = text)
+      : (warning.innerHTML = "Bitte gib ein Nachricht ein");
 }
 
-console.log(message);
