fba04c6a21506520143ea838d1355d83e83a94e6
Reazy96 <resul.sadriu@outlook.com>
2024-02-05 18:19:15 +0100

    lvl 1_8 done

---
+++ Arrow Function/Level 1_8/script.js
@@ -0,0 +1,13 @@
+let age = (yearBorn) => {
+  let myAge = yearBorn - 1996;
+  return myAge;
+};
+
+console.log(age(2024));
+
+let otherAge = (year1, year2) => {
+  let alterDiff = year1 - year2;
+  return alterDiff;
+};
+
+console.log(otherAge(30, 28));
