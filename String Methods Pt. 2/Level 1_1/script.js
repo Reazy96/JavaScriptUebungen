a0d2839d2726e472f5f2d4f8082877a30b09ebea
Reazy96 <resul.sadriu@outlook.com>
2024-02-05 11:51:50 +0100

    lvl 1_1 done

---
+++ String Methods Pt. 2/Level 1_1/script.js
@@ -0,0 +1,9 @@
+let firstName = "Resul";
+let lastName = "Sadriu";
+
+console.log(firstName.length);
+console.log(lastName.length);
+
+const fullName = `${firstName}${lastName}`;
+
+console.log(fullName.length);
