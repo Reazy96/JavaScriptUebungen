382d1cc7a575fa962a9e02b73d76582172863f96
Reazy96 <resul.sadriu@outlook.com>
2024-02-05 13:00:15 +0100

    lvl 1_7 done

---
+++ String Methods Pt. 2/Level 1_7/script.js
@@ -0,0 +1,10 @@
+const text0 = "Sam is good at codingschool";
+
+let text1 = text0.replace("good at codingschool", "bad at school");
+document.write(text1 + "<br>");
+
+let text2 = text1.replace("Sam is bad at school", "Susi is good at school");
+document.write(text2 + "<br>");
+
+let text3 = text0.replace("codingschool", "tennis");
+document.write(text3 + "<br>");
