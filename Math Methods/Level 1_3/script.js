8aaa3cabf9534040069cd572b39af41548a10a2d
Reazy96 <resul.sadriu@outlook.com>
2024-02-06 12:26:41 +0100

    lvl 1_3 done

---
+++ Math Methods/Level 1_3/script.js
@@ -0,0 +1,8 @@
+let randomNum1 = Math.random();
+console.log(randomNum1);
+
+let randomNum2 = Math.floor(Math.random() * 10);
+console.log(randomNum2);
+
+let randomNum3 = Math.floor(Math.random() * 100);
+console.log(randomNum3);
