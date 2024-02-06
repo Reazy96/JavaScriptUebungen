5c66b9cf67d2bcec354e04215831d13eef0ef689
Reazy96 <resul.sadriu@outlook.com>
2024-02-06 12:05:21 +0100

    lvl 1_2 done

---
+++ Math Methods/Level 1_2/script.js
@@ -0,0 +1,18 @@
+const toRound1 = () => {
+  let array = [3.14, 193.4464, 0.8596433607, -2.940629089];
+  console.log(Math.round(array));
+};
+toRound1();
+
+const toRound2 = () => {
+  let zahl1 = 3.14;
+  console.log(Math.round(zahl1));
+  let zahl2 = 193.4464;
+  console.log(Math.round(zahl2));
+  let zahl3 = 0.8596433607;
+  console.log(Math.round(zahl3));
+  let zahl4 = -2.940629089;
+  console.log(Math.round(zahl4));
+};
+
+toRound2();
