6ee38a08669f22886627d54b5da0b1afec51d64a
Reazy96 <resul.sadriu@outlook.com>
2024-02-07 12:34:29 +0100

    lvl 1_1 done

---
+++ Arrays/Level 1_1 - Level 1_9/script.js
@@ -0,0 +1,9 @@
+const person = ["albanian", "27yrs", "gamer"];
+const friends = ["GubiFortnite", "NiekBeats", "SowiFortnite"];
+const favoriteFood = ["Reis mit Chicken", "Sushi", "Kuchen"];
+
+console.log(person, friends, favoriteFood);
+
+console.table(person);
+console.table(friends);
+console.table(favoriteFood);
